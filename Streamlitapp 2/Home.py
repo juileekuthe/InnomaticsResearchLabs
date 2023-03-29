87a4626cdc7bd24569fdf36a7eecf41d8307db2f
Juilee Kuthe <117886747+juileekuthe@users.noreply.github.com>
2023-03-29 15:50:33 +0530

    Create Home.py

---
+++ Streamlitapp 2/Home.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.title(":red[Hello] Everyone!! :woman-raising-hand: ")
+
+st.header("My name is :green[Juilee Kuthe], I developed a dashboards using Streamlit")
+
+st.header("I create my first streamlit app")
+
+st.subheader("with the help of :blue[Kanav Bansal Sir] I create first streamlit app ")
+             
+st.subheader("This is our first assignment project")
+
+st.subheader("You can access my project")
+
+st.write("Github [link] (https://github.com/juileekuthe/DataScienceInternship)")
+
+st.subheader("Lets Connect")
+
+st.write("Linkedin [link] (https://www.linkedin.com/in/juilee-kuthe-857454242/)")
+
+
